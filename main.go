f95ecee52f059caf6e66d12ddb5788f30cac6b2b
suraj-2584 <surajpbksr@gmail.com>
2025-10-15 18:07:25 +0530

    allow cors to localhost for testing

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"deps/url-shortener/internal/handlers"
 	"deps/url-shortener/internal/utils"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	utils.LoadSecrets()
 	router := gin.Default()
+	router.Use()
 	router.GET("/:id", handlers.ShortenendUrlHandler)
 	router.POST("/shorten", handlers.ShortenUrlHandler)
 	router.GET("/health", handlers.HealthHandler)
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost"}}))
 	router.Run(":5001")
 }
